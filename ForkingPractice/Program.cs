4b35af8ed0824b0e420dda2cc5f247eae1210a3b
Travis K <traviskean04@live.com>
2015-07-27 11:04:41 -0400

    Simple Program to test forking repositories

---
+++ ForkingPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Travis";
+
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
